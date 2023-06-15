1fd64b185aa6523975b7542ba8e3cf2eea9b3cdc
Михаил <solomein-m@mail.ru>
2023-06-15 22:05:28 +0500

    fixed-bug-with-background

--- Scripts/AudioManager.cs
+++ Scripts/AudioManager.cs
@@ -6,15 +6,11 @@ using UnityEngine.UI;
 public class AudioManager : MonoBehaviour
 {
     public AudioSource First;
-    public AudioSource Second;
-    public AudioSource Third;
     public static bool IsPlaying;
-    public static int Index;
 
     public void Awake()
     {
         IsPlaying = false;
-        Index = 0;
     }
 
     public void PlayClip()
@@ -22,32 +18,12 @@ public class AudioManager : MonoBehaviour
         if (IsPlaying)
         {
             IsPlaying = false;
-            Index = (Index + 1) % 3;
             First.enabled = false;
-            Second.enabled = false;
-            Third.enabled = false;
         }
         else
         {
             IsPlaying = true;
-            switch (Index)
-            {
-                case 0:
-                    First.enabled = true;
-                    Second.enabled = false;
-                    Third.enabled = false;
-                    break;
-                case 1:
-                    First.enabled = false;
-                    Second.enabled = true;
-                    Third.enabled = false;
-                    break;
-                case 2:
-                    First.enabled = false;
-                    Second.enabled = false;
-                    Third.enabled = true;
-                    break;
-            }
+            First.enabled = true;
         }
     }
 }
